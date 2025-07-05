892e13b125e36441b2a528c2e4f15b1383b29221
Matheus-Nobre <matheusipsnobre@gmail.com>
2025-07-05 18:35:40 -0300

    CRUD completo

--- NestJS_Curso/conceitos_nest/src/recados/recados.controller.ts
+++ NestJS_Curso/conceitos_nest/src/recados/recados.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+
     @Get()
     findAll(){
         return "Essa rota enontra todos os Recados"
@@ -10,5 +11,23 @@ export class RecadosController {
     @Get(':id')
     findOne(@Param('id') id : string){
         return `Essa rota enontra o Recado e o ID: ${id}`
+    }
+
+    @Post()
+    create(@Body() body:any){
+        return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: any){
+        return {
+            id,
+            ...body
+        }
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return `Essa rota APAGA o recado ID ${id}`;
     }
 }
