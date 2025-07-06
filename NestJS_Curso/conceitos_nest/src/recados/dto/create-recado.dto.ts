0438a2672fd6b4be47cf076b1f37760f01bbc326
Matheus-Nobre <matheusipsnobre@gmail.com>
2025-07-06 14:52:25 -0300

    Validator

--- NestJS_Curso/conceitos_nest/src/recados/dto/create-recado.dto.ts
+++ NestJS_Curso/conceitos_nest/src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,21 @@
+import { IsEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateRecadoDto {
+  @IsString()
+  @IsEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly texto: string;
+
+  @IsString()
+  @IsEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly de: string;
+
+  @IsString()
+  @IsEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly para: string;
 }
