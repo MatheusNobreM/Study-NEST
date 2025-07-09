d9ccdd2fc71eadd1fa9063b48ed7e1b9ee9ea9a8
Matheus-Nobre <matheusipsnobre@gmail.com>
2025-07-08 23:44:41 -0300

    create pessoa

--- NestJS_Curso/conceitos_nest/src/pessoas/dto/create-pessoa.dto.ts
+++ NestJS_Curso/conceitos_nest/src/pessoas/dto/create-pessoa.dto.ts
@@ -1,13 +1,23 @@
-import { IsEmail, IsStrongPassword } from "class-validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreatePessoaDto {
   @IsEmail()
   email: string;
-  
-  @IsStrongPassword({
-    
-  })
-  passwordHash: string;
-  
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
   nome: string;
 }
--- NestJS_Curso/conceitos_nest/src/pessoas/entities/pessoa.entity.ts
+++ NestJS_Curso/conceitos_nest/src/pessoas/entities/pessoa.entity.ts
@@ -1,24 +1,30 @@
-import { IsEmail } from "class-validator";
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { IsEmail } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Pessoa {
   @PrimaryGeneratedColumn()
   id: number;
-  
-  @Column()
+
+  @Column({ unique: true})
   @IsEmail()
   email: string;
 
-  @Column({length: 255})
+  @Column({ length: 255 })
   passwordHash: string;
 
-  @Column({length: 100})
+  @Column({ length: 100 })
   nome: string;
 
   @CreateDateColumn()
   createdAt?: Date;
-  
+
   @UpdateDateColumn()
   updateAt?: Date;
 }
