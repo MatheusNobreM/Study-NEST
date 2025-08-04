6836a7c95ae519649f9ae2323d32e87f2667dbf1
Matheus-Nobre <matheusipsnobre@gmail.com>
2025-08-04 18:52:15 -0300

    interceptor error

---
+++ NestJS_Curso/conceitos_nest/src/common/interceptors/error-handling.interceptor.ts
@@ -0,0 +1,25 @@
+import { BadRequestException, CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { catchError, throwError } from "rxjs";
+
+export class ErrorHandlingInterceptor implements NestInterceptor {
+  async intercept(
+    context: ExecutionContext,
+     next: CallHandler<any>
+    ){
+      console.log('ErrorHandlingInterceptor executado');
+
+    return next.handle().pipe(
+      catchError(error => {
+        console.log(error.name);
+        console.log(error.message);
+        return throwError(() => {
+          if (error.name === 'NotFoundException') {
+            return new BadRequestException(error.message);
+          }
+
+          return new BadRequestException('Ocorreu um erro inesperado.');
+        });
+      })
+    );
+  }
+}
--- NestJS_Curso/conceitos_nest/src/recados/recados.controller.ts
+++ NestJS_Curso/conceitos_nest/src/recados/recados.controller.ts
@@ -20,13 +20,14 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
 import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 import { TimingConnectionInterceptor } from 'src/common/interceptors/timing-connection.interceptor';
+import { ErrorHandlingInterceptor } from 'src/common/interceptors/error-handling.interceptor';
 
 @Controller('recados')
 @UsePipes(ParseIntIdPipe)
 export class RecadosController {
   constructor(private readonly recadosService: RecadosService) {}
 
-  @UseInterceptors(TimingConnectionInterceptor)
+  @UseInterceptors(TimingConnectionInterceptor, ErrorHandlingInterceptor)
   @HttpCode(HttpStatus.OK)
   @Get()
   async findAll(@Query() paginationDto: PaginationDto) {
@@ -36,7 +37,7 @@ export class RecadosController {
   }
 
   @Get(':id')
-  @UseInterceptors(AddHeaderInterceptor)//cabeçalho
+  @UseInterceptors(AddHeaderInterceptor, ErrorHandlingInterceptor)//cabeçalho
   findOne(@Param('id') id: number) {
     return this.recadosService.findOne(id);
   }
